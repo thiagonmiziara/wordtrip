28caaa33cbab92fe37f60df7bf6f0be8b74805b1
thiago miziara <thiagonmiziara@gmail.com>
2021-05-13 19:41:26 -0300

    add content no component dividerline

--- src/components/DividerLine/Index.tsx
+++ src/components/DividerLine/Index.tsx
@@ -1,14 +1,29 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Heading } from "@chakra-ui/react";
 
 const DivideLine = () => {
   return (
-    <Box
-      w={["60px", "90px"]}
-      mx="auto"
-      h="2px"
-      bg="gray.700"
-      my={["9", "20"]}
-    />
+    <>
+      <Box
+        w={["60px", "90px"]}
+        mx="auto"
+        h="2px"
+        bg="gray.700"
+        my={["9", "20"]}
+      />
+
+      <Box>
+        <Heading 
+          textAlign="center"
+          fontWeight="500"
+          fontSize={["lg","3xl","4xl"]}
+          mb={["5","14"]}
+        >
+          Vamos nessa?
+          <br />
+          Então escolha seu continente
+        </Heading>
+      </Box>
+    </>
   );
 };
 
